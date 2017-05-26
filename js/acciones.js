733450eaca1de72a6faea2e758db46eddfda4b76
luisk5 <ldaviddj4@gmail.com>
2017-05-26 08:54:29 -0500

    11subida

--- js/acciones.js
+++ js/acciones.js
@@ -24,18 +24,18 @@ $('#watch').on('click',function (){
 		maximumAge:3600000
 	}
 	
-	var watchID = navigator.geolocation.getCurrentPosition(onSuccesss,onError,options);
+	var watchID=navigator.geolocation.getCurrentPosition(onSuccesss,onError,options);
 	
 	function onSuccess(position){
 		
-	  alert('Latitude:' +position.coords.latitude  +'\n'+
-		'Longitude:' +position.coords.longitude  +'\n'+
-		'Altitude:' +position.coords.altitude  +'\n'+
-		'Accuracy:' +position.coords.accuracy  +'\n'+
-		'Altitude Accuracy:' +position.coords.altitudeAccuracy  +'\n'+
-		'Heading:' +position.coords.heading  +'\n'+
-		'Speed:' +position.coords.speed  +'\n'+
-		'Timestamp:' +position.timestamp  +'\n');
+	  alert('Latitude:'    +position.coords.latitude  +'\n'+
+		'Longitude:'    +position.coords.longitude  +'\n'+
+		'Altitude:'    +position.coords.altitude  +'\n'+
+		'Accuracy:'    +position.coords.accuracy  +'\n'+
+		'Altitude Accuracy:'    +position.coords.altitudeAccuracy  +'\n'+
+		'Heading:'    +position.coords.heading  +'\n'+
+		'Speed:'    +position.coords.speed  +'\n'+
+		'Timestamp:'    +position.timestamp  +'\n');
 	};
 	
 	function onError(error){
