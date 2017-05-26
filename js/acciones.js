557eb18f3478ec303db76da88af4054b9bee400a
luisk5 <ldaviddj4@gmail.com>
2017-05-26 08:51:02 -0500

    10subida

--- js/acciones.js
+++ js/acciones.js
@@ -21,37 +21,37 @@ $('#watch').on('click',function (){
 		
 	var options={
 		enableHighAccuracy :true,
-		maximumAge: 3600000
+		maximumAge:3600000
 	}
 	
 	var watchID = navigator.geolocation.getCurrentPosition(onSuccesss,onError,options);
 	
 	function onSuccess(position){
 		
-	  alert('Latitude:'  +position.coords.latitude  +'\n'+
-		'Longitude:'  +position.coords.longitude  +'\n'+
-		'Altitude:'  +position.coords.altitude  +'\n'+
-		'Accuracy:'  +position.coords.accuracy  +'\n'+
-		'Altitude Accuracy:'  +position.coords.altitudeAccuracy  +'\n'+
-		'Heading:'  +position.coords.heading  +'\n'+
-		'Speed:'  +position.coords.speed  +'\n'+
-		'Timestamp:'  +position.timestamp  +'\n');
+	  alert('Latitude:' +position.coords.latitude  +'\n'+
+		'Longitude:' +position.coords.longitude  +'\n'+
+		'Altitude:' +position.coords.altitude  +'\n'+
+		'Accuracy:' +position.coords.accuracy  +'\n'+
+		'Altitude Accuracy:' +position.coords.altitudeAccuracy  +'\n'+
+		'Heading:' +position.coords.heading  +'\n'+
+		'Speed:' +position.coords.speed  +'\n'+
+		'Timestamp:' +position.timestamp  +'\n');
 	};
 	
 	function onError(error){
-		alert('code:' +error.code +'\n'+'message'+error.message+'\n');
+		alert('code:'+error.code +'\n'+'message'+error.message+'\n');
 	}
 }
 
 function watchPosition(){
 	
 	var options={
-		maximumAge :3600000,
-		timeout : 3000,
+		maximumAge:3600000,
+		timeout:3000,
 		enableHighAccuracy :true,
 	}
 	
-	var watchID= navigator.geolocation.watchPosition(onSuccess,onError,options);
+	var watchID=navigator.geolocation.watchPosition(onSuccess,onError,options);
 	
 	function onSucces(position){
 		
@@ -66,7 +66,7 @@ $('#timestamp').html(position.timestamp);
 	};
 	
 	function onError(error){
-		alert('code:'  +error.code  +'\n'+'message:' + error.message +'\n');
+		alert('code:' +error.code +'\n'+'message:'+ error.message +'\n');
 	}
 	
 }
