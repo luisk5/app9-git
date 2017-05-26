21705604995f58b63b779671ca2e5668821885d3
luisk5 <ldaviddj4@gmail.com>
2017-05-26 09:00:04 -0500

    12subida

--- js/acciones.js
+++ js/acciones.js
@@ -7,35 +7,35 @@ $(document).ready(function (e) {
 
 function  onDeviceReady() {
 
-$('#posicion').on('click',function (){
+$('#posicion').on('click',function(){
 	getPosition ();
 	});
 
-$('#watch').on('click',function (){
+$('#watch').on('click',function(){
 	watchPosition ();
 });
 
 }
 
-	function getPosition() {
+	function getPosition(){
 		
 	var options={
 		enableHighAccuracy :true,
-		maximumAge:3600000
+		maximumAge: 3600000
 	}
 	
-	var watchID=navigator.geolocation.getCurrentPosition(onSuccesss,onError,options);
+	var watchID = navigator.geolocation.getCurrentPosition(onSuccesss,onError,options);
 	
 	function onSuccess(position){
 		
-	  alert('Latitude:'    +position.coords.latitude  +'\n'+
-		'Longitude:'    +position.coords.longitude  +'\n'+
-		'Altitude:'    +position.coords.altitude  +'\n'+
-		'Accuracy:'    +position.coords.accuracy  +'\n'+
-		'Altitude Accuracy:'    +position.coords.altitudeAccuracy  +'\n'+
-		'Heading:'    +position.coords.heading  +'\n'+
-		'Speed:'    +position.coords.speed  +'\n'+
-		'Timestamp:'    +position.timestamp  +'\n');
+	  alert('Latitude:'    +position.coords.latitude    +'\n'+
+		'Longitude:'    +position.coords.longitude    +'\n'+
+		'Altitude:'    +position.coords.altitude    +'\n'+
+		'Accuracy:'     +position.coords.accuracy       +'\n'+
+		'Altitude Accuracy:'+position.coords.altitudeAccuracy +'\n'+
+		'Heading:'      +position.coords.heading        +'\n'+
+		'Speed:'    +position.coords.speed            +'\n'+
+		'Timestamp:'    +position.timestamp            +'\n');
 	};
 	
 	function onError(error){
